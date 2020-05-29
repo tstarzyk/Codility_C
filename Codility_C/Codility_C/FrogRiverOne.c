e528df3271ef5bb86fb9e498507dcdbf1bfa4513
tstarzyk <tomek.starzyk@gmail.com>
2020-05-29 19:04:04 +0100

    Removed debug messages
    Removed printf

--- Codility_C/Codility_C/FrogRiverOne.c
+++ Codility_C/Codility_C/FrogRiverOne.c
@@ -14,21 +14,13 @@ int FrogRiverOneSolution(int X, int A[], int N)
 			return -1;
 		}
 	}
-	for (int k = 0; k < N; k++)
-	{
-		printf("%d, ", A[k]);
-	}
-	printf("\r\n");
 
 	status = (int*)calloc(X, sizeof(int));
 	int cnt = 0;
 	for (int i = 0; i < N; i++) 
 	{
-		printf("A[i] %d \r\n", A[i]);
 		if (status[A[i] - 1] == 0) 
 		{
-			printf("status[A[i]] %d \r\n", status[A[i]]);
-			printf("status[A[i] - 1] %d\r\n", status[A[i] - 1]);
 			cnt++;
 			status[A[i] - 1] = 1;
 		}
